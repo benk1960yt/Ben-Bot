1fb0af7cb608430d8b236e70f2a188d0ef2d20bb
benk1960yt <40948605+benk1960yt@users.noreply.github.com>
2018-07-08 14:48:37 +0300

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,21 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+const perfix = "$"
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', msg => {
+  if (msg.content === perfix + 'help') {
+      msg.reply('Please check your direct messages');
+      msg.author.sendMessage("The commands is: $help for commands help, $crator to see who create this bot")
+  }
+});
+client.on('message', msg => {
+if (msg.content === perfix + 'creator') {
+msg.reply('discord bot creator is: Ben');
+  }
+});
+
+client.login(process.env.BOT_TOKEN);
